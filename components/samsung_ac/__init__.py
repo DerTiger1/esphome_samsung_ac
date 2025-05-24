e625006bf6b2c01039e72832b6bf84abc14fe160
DerTiger1 <95141154+DerTiger1@users.noreply.github.com>
2025-05-24 16:29:00 +0200

    Update __init__.py

--- components/samsung_ac/__init__.py
+++ components/samsung_ac/__init__.py
@@ -333,7 +333,7 @@ CONFIG_SCHEMA = (
             cv.Optional(CONF_DEBUG_LOG_MESSAGES, default=False): cv.boolean,
             cv.Optional(CONF_DEBUG_LOG_MESSAGES_RAW, default=False): cv.boolean,
             cv.Optional(CONF_NON_NASA_KEEPALIVE, default=False): cv.boolean,
-            cv.Optional(CONF_DEBUG_LOGcv.UNDEFINEDINED_MESSAGES, default=False): cv.boolean,
+            cv.Optional(CONF_DEBUG_LOG_UNDEFINED_MESSAGES, default=False): cv.boolean,
             cv.Optional(CONF_CAPABILITIES): CAPABILITIES_SCHEMA,
             cv.Required(CONF_DEVICES): cv.ensure_list(DEVICE_SCHEMA),
         }
